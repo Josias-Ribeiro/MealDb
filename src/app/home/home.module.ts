cabd699efd73cf6d9119f6355860c1ee40032093
Josias-Ribeiro <josias_ribeiro90@hotmail.com>
2022-05-14 02:13:32 -0300

    MDB - Ajustes em home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
+import { CardModule } from 'src/shared/cards/cards.module';
 import { MaterialModule } from 'src/shared/material.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -11,7 +12,7 @@ import { HomeHttpService } from './services/home-http.service';
 
 
 @NgModule({
-    imports: [CommonModule, HomeRoutingModule, HttpClientModule, FlexLayoutModule, MaterialModule],
+    imports: [CommonModule, HomeRoutingModule, HttpClientModule, FlexLayoutModule, MaterialModule, CardModule],
     exports: [HomeComponent],
     declarations: [HomeComponent],
     providers: [HomeHttpService],
--- src/shared/cards/card.component.ts
+++ src/shared/cards/card.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'mdb-card',
-    templateUrl: './view/card.component.html'
+    templateUrl: './view/card.component.html',
+    styleUrls: ['./view/card.component.scss']
     
 })
 
 export class CardComponent implements OnInit {
+    @Input() tituloCabecalho: string;
+    @Input() conteudoCard: any;
+
     constructor() { }
 
     ngOnInit() { }
